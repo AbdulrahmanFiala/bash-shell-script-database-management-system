e59b25c6c7bfc997fac430b47d32285ff2c2b121
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2023-09-15 23:51:23 +0300

    Adding a break to the menue options

--- connect.sh
+++ connect.sh
@@ -12,24 +12,31 @@ if [ -d "$DB_PATH" ]; then
         case $choice in
         "Create Table")
             /home/commands/creat_table.sh
+            break
             ;;
         "List Tables")
             /home/commands/list_tables.sh
+            break
             ;;
         "Drop Table")
             /home/commands/drop_tables.sh
+            break
             ;;
         "Insert into Table")
             /home/commands/insert_into_table.sh
+            break
             ;;
         "Select From Table")
             /home/commands/select_from_table.sh
+            break
             ;;
         "Delete From Table")
             /home/commands/delete_from_table.sh
+            break
             ;;
         "Update Table")
             /home/commands/update_table.sh
+            break
             ;;
         *)
             echo "$REPLY" is not one of the choices.
