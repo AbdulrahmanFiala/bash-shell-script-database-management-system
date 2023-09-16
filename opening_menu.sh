8d0a05053bb78244d371be135e912796131815b5
Abdulaziz-elitecoder <abdulaziz.muhammed.elnoti@gmail.com>
2023-09-16 16:10:30 +0300

    added 3 scripts to the repo : opening_menu.sh :     shows the user the main menu thats mainly has the key options for the DBMS creating_database.sh :     responsible mainly for creating databases if does not exist deleting_database.sh :     responsible mainly for deleting databases if does exist

---
+++ opening_menu.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#checking if there is a directory containing all databases
+
+if ! [[ -d ./MyDBs ]]; then
+	mkdir ./MyDBs
+fi
+
+#Starting the main menu
+
+#prompt name
+PS3="Type the number of your choice: "
+
+#select from the opening menu
+select choice in "create a database" "list all databases" "connect to a database" "delete a databse" "clear screen except menu" "exit"; do
+case $choice in
+
+"create a database")  	   	echo -e "You are creating a new database\n"
+				. ./creating_database.sh ;;
+
+"list all databases")	  	echo -e "$(ls ./MyDBs)\n" ;;
+
+"connect to a database") 	echo -e "You are connecting to a databse\n"
+				. ./connect.sh;;
+
+"delete a databse")		echo -e "You are now deleting a databse\n"
+				. ./deleting_database.sh;;
+
+"clear screen except menu")     echo -e "Screen cleared\n"
+				. ./clear.sh;;
+
+"exit")				clear
+				echo -e "Thanks for using our DBMS\n"
+				exit;;
+
+*) 				echo "Invalid choice, please try again"
+				. ./opening_menu.sh;;
+esac
+done
