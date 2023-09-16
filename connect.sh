673ea94c4f069a6025a26d7333e47cfa0730787a
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2023-09-17 00:40:08 +0300

    Update connect.sh
    modified the path (again!)

--- connect.sh
+++ connect.sh
@@ -1,42 +1,40 @@
 #!/usr/bin/bash
 read -r -p "What's the name of the Database that you want to connect to: " DB_NAME
 
-DB_PATH="/home/fiala/MyDBs/$DB_NAME"
+DB_PATH="./MyDBs/$DB_NAME"
 
 if [ -d "$DB_PATH" ]; then
     cd "$DB_PATH"
     PS3="Select an option: "
     options=("Create Table" "List Tables" "Drop Table" "Insert into Table" "Select From Table" "Delete From Table" "Update Table")
-
     select choice in "${options[@]}"; do
         case $choice in
         "Create Table")
-            pwd
-            "/home/fiala/scripts/create_table.sh"
+            "./../../scripts/create_table.sh"
             break
             ;;
         "List Tables")
-            "/home/fiala/scripts/list_tables.sh"
+            "./../../scripts/list_tables.sh"
             break
             ;;
         "Drop Table")
-            "/home/fiala/scripts/drop_tables.sh"
+            "./../../scripts/drop_table.sh"
             break
             ;;
         "Insert into Table")
-            "/home//fiala/scripts/insert_into_table.sh"
+            "./../../scripts/insert_into_table.sh"
             break
             ;;
         "Select From Table")
-            "/home/fiala/scripts/select_from_table.sh"
+            "./../../scripts/select_from_table.sh"
             break
             ;;
         "Delete From Table")
-            "/home/fiala/scripts/delete_from_table.sh"
+            "./../../scripts/delete_from_table.sh"
             break
             ;;
         "Update Table")
-            "/home/fiala/scripts/update_table.sh"
+            "./../../scripts/update_table.sh"
             break
             ;;
         *)
