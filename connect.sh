b2ecb6b71977eb1cd97f78695c686b7710203e76
Abdulaziz-elitecoder <abdulaziz.muhammed.elnoti@gmail.com>
2023-09-17 00:53:13 +0300

    modified: connect.sh

--- connect.sh
+++ connect.sh
@@ -12,31 +12,31 @@ if [ -d "$DB_PATH" ]; then
         case $choice in
         "Create Table")
             pwd
-            "./scripts/create_table.sh"
+            "../scripts/create_table.sh"
             break
             ;;
         "List Tables")
-            "./scripts/list_tables.sh"
+            "../scripts/list_tables.sh"
             break
             ;;
         "Drop Table")
-            "./scripts/drop_tables.sh"
+            "../scripts/drop_tables.sh"
             break
             ;;
         "Insert into Table")
-            "./scripts/insert_into_table.sh"
+            "../scripts/insert_into_table.sh"
             break
             ;;
         "Select From Table")
-            "./scripts/select_from_table.sh"
+            "../scripts/select_from_table.sh"
             break
             ;;
         "Delete From Table")
-            "./scripts/delete_record.sh"
+            "../scripts/delete_record.sh"
             break
             ;;
         "Update Table")
-            "./scripts/update_table.sh"
+            "../scripts/update_table.sh"
             break
             ;;
         *)
