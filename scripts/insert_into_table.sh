bed42c646dee7d00d2f6e13466c4c0f910bb150f
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2023-09-17 16:09:56 +0300

    edited the function calls

--- scripts/insert_into_table.sh
+++ scripts/insert_into_table.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-function primary_key_check() {
+function primary_key_vaildate() {
     key=$(awk -F "⋮" -v col="$i" 'NR == 1 {print $col}' "$(pwd)/$option" | awk -F "," '{print $3}')
-
     if [[ $key == *"(PRIMARY_KEY)"* ]]; then
         previous_values=($(awk -F "⋮" -v col="$i" 'NR > 2 {gsub(/^[ \t]+|[ \t]+$/,"",$col); print $col}' "$(pwd)/$option"))
 
         for item in "${previous_values[@]}"; do
             while [[ "$item" == "$data" ]]; do
                 read -r -p "You can only enter unique values since this column is a primary key. Please enter a different value: " data
-                vaildate_int
+                # to validate the user input after entering a unique value
+                if [[ "$data_type" == *'int'* ]]; then
+                    vaildate_int
+
+                elif [[ "$data_type" == *'string'* ]]; then
+                    vaildate_string
+
+                else
+                    vaildate_boolean
+                fi
             done
         done
     fi
@@ -50,12 +58,12 @@ select option in "${list_of_tables[@]}"; do
 
         for ((i = 1; i <= no_of_columns; i++)); do
             read -r -p "What do you want to insert in column number $i: " data
-            data_var=$(awk -F "⋮" -v col="$i" 'NR == 1 {print $col}' "$(pwd)/$option" | awk -F "," '{print $2}')
+            data_type=$(awk -F "⋮" -v col="$i" 'NR == 1 {print $col}' "$(pwd)/$option" | awk -F "," '{print $2}')
 
-            if [[ "$data_var" == *'int'* ]]; then
+            if [[ "$data_type" == *'int'* ]]; then
                 vaildate_int
 
-            elif [[ "$data_var" == *'string'* ]]; then
+            elif [[ "$data_type" == *'string'* ]]; then
                 vaildate_string
 
             else
