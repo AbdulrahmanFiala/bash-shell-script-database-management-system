5f062838661e605af05fb5f8ef933d0fa93b7f13
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2023-09-18 23:28:55 +0300

    Update opening_menue.sh

--- opening_menu.sh
+++ opening_menu.sh
@@ -13,28 +13,40 @@ PS3="Type the number of your choice: "
 
 #select from the opening menu
 select choice in "create a database" "list all databases" "connect to a database" "delete a databse" "clear screen except menu" "exit"; do
-case $choice in
+	case $choice in
 
-"create a database")  	   	echo -e "You are creating a new database\n"
-				. ./creating_database.sh ;;
+	"create a database")
+		echo -e "You are creating a new database\n"
+		. ./creating_database.sh
+		;;
 
-"list all databases")	  	echo -e "$(ls ./MyDBs)\n" ;;
+	"list all databases") echo -e "$(ls ./MyDBs)\n" ;;
 
-"connect to a database") 	echo -e "You are connecting to a databse\n"
-				. ./connect.sh;;
+	"connect to a database")
+		echo -e "You are connecting to a databse\n"
+		. ./connect.sh
+		;;
 
-"delete a databse")		echo -e "You are now deleting a databse\n"
-				. ./deleting_database.sh;;
+	"delete a databse")
+		echo -e "You are now deleting a databse\n"
+		. ./deleting_database.sh
+		;;
 
-"clear screen except menu")     echo -e "Screen cleared\n"
-				. ./clear.sh;;
+	"clear screen except menu")
+		echo -e "Screen cleared\n"
+		. ./clear.sh
+		;;
 
-"exit")				clear
-				echo -e "Thanks for using our DBMS\n"
-				exit;;
+	"exit")
+		clear
+		echo -e "Thanks for using our DBMS\n"
+		exit
+		;;
 
-*) 				echo "Invalid choice, please try again"
-				cd ../..
-				./opening_menu.sh;;
-esac
+	*)
+		echo "Invalid choice, please try again"
+		cd ../..
+		./opening_menu.sh
+		;;
+	esac
 done
