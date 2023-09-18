b8d597a38d822e6e07a8747f665792ce2abfb54b
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2023-09-18 18:36:20 +0300

    added a select menu instead of asking the user for an input

--- connect.sh
+++ connect.sh
@@ -1,53 +1,55 @@
 #!/usr/bin/bash
-read -r -p "What's the name of the Database that you want to connect to: " DB_NAME
-export DB_NAME
-DB_PATH="./MyDBs/$DB_NAME"
+readarray -t list_of_databases < <(ls "$(pwd)/MyDBs")
+PS3="Which database do you want to connect to: "
 
-if [ -d "$DB_PATH" ]; then
-    cd "$DB_PATH"
-    PS3="Select an option: "
-    options=("Create Table" "List Tables" "Drop Table" "Insert into Table" "Select From Table" "Delete From Table" "Update Table" "Back to the main menue")
+select db in "${list_of_databases[@]}"; do
+    if [[ "$REPLY" =~ ^[0-9]+$ && "$REPLY" -ge 1 && "$REPLY" -le ${#list_of_databases[@]} ]]; then
 
-    select choice in "${options[@]}"; do
-        case $choice in
-        "Create Table")
-            "../../scripts/create_table.sh"
-            break
-            ;;
-        "List Tables")
-            "../../scripts/list_tables.sh"
-            break
-            ;;
-        "Drop Table")
-            "../../scripts/drop_table.sh"
-            break
-            ;;
-        "Insert into Table")
-            "../../scripts/insert_into_table.sh"
-            break
-            ;;
-        "Select From Table")
-            "../../scripts/select_from_table.sh"
-            break
-            ;;
-        "Delete From Table")
-            "../../scripts/delete_record.sh"
-            break
-            ;;
-        "Update Table")
-            "../../scripts/update_table.sh"
-            break
-            ;;
-        "Back to the main menue")
-            "../../opening_menu.sh"
-            break
-            ;;
-        *)
-            echo "$REPLY" is not one of the choices.
-            ;;
-        esac
-    done
+        cd "$(pwd)/MyDBs/$db"
+        PS3="Select an operating on the database $db: "
+        options=("Create Table" "List Tables" "Drop Table" "Insert into Table" "Select From Table" "Delete From Table" "Update Table" "Back to the main menue")
 
-else
-    echo "$DB_NAME Database doesn't exist"
-fi
+        select choice in "${options[@]}"; do
+            case $choice in
+            "Create Table")
+                "../../scripts/create_table.sh"
+                break
+                ;;
+            "List Tables")
+                "../../scripts/list_tables.sh"
+                break
+                ;;
+            "Drop Table")
+                "../../scripts/drop_table.sh"
+                break
+                ;;
+            "Insert into Table")
+                "../../scripts/insert_into_table.sh"
+                break
+                ;;
+            "Select From Table")
+                "../../scripts/select_from_table.sh"
+                break
+                ;;
+            "Delete From Table")
+                "../../scripts/delete_record.sh"
+                break
+                ;;
+            "Update Table")
+                "../../scripts/update_table.sh"
+                break
+                ;;
+            "Back to the main menue")
+                "../../opening_menu.sh"
+                break
+                ;;
+            *)
+                echo "$REPLY" is not one of the choices.
+                ;;
+            esac
+        done
+
+    else
+        echo "$DB_NAME Database doesn't exist"
+    fi
+done
