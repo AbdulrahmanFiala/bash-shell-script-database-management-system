957a247e88892d1f4c8c12c0e0eb485d755b6af6
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2023-09-18 13:50:43 +0300

    Redoing the delete_record.sh
    Implementing a different approach for deleting records that's more user friendly.

--- scripts/delete_record.sh
+++ scripts/delete_record.sh
@@ -1,45 +1,52 @@
 #!/bin/bash
 
-cd ./MyDBs/$database/ 2> /dev/null
-echo "Please enter table name to delete from: "
-read table 2> /dev/null
-if [[ -f $table ]]
-then
-        #print table header
-        awk 'BEGIN{FS="⋮"}{if (NR==1) {for(i=1;i<=NF;i++){printf "----||----"$i}{print "---|"}}}' $table
-        echo  "Enter Column name:"
-        read field 2>> /dev/null
+# convert the list of tables files of the DB to an array to iterate over them.
+readarray -t list_of_tables < <(ls "$(pwd)")
 
-        # get the field number
-        fieldNumber=$(awk 'BEGIN{FS="⋮"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$field'") print i }}}' $table )
-        if [[ $fieldNumber == "" ]]
-        then
-            echo "Not Found"
-            cd ../..
-            ./connect.sh            
+PS3="Which table do you want to delete from: "
+
+# menue to select which table to select from
+select option in "${list_of_tables[@]}"; do
+
+    # validate the user input to be a number that's greater than or equal 1 and less than or equal the length of the array
+    if [[ "$REPLY" =~ ^[0-9]+$ && "$REPLY" -ge 1 && "$REPLY" -le ${#list_of_tables[@]} ]]; then
+
+        # check if the table is empty
+        if [ ! -s "$(pwd)"/"$option" ]; then
+            echo "$option" table is an empty table. You cannont delete from an empty table.
+
         else
-            echo "Enter Value:"
-            read value 2>> /dev/null
-            result=$(awk 'BEGIN{FS="⋮"}{if ($'$fieldNumber'=="'$value'") print $'$fieldNumber'}' $table 2>> /dev/null)
+            {
+                table_lines=()
+                starting_line=3
+                while IFS= read -r line; do
+                    table_lines+=("$line")
+                    starting_line=$((starting_line + 1))
+                done < <(tail -n +$starting_line "$(pwd)"/"$option")
 
-            if [[ $result == "" ]]
-            then
-                echo "Value Not Found"
-                cd ../..
-                ./connect.sh                
-            else
-                # get the record number to be deleted
-                NR=$(awk 'BEGIN{FS="⋮"}{if ($'$fieldNumber'=="'$value'") print NR}' $table 2>> /dev/null)
-                # delete the record
-                sed -i ''$NR'd' $table 2>> /dev/null
-                echo "Row Deleted Successfully"
-                cd ../..
-                ./connect.sh
+                PS3="Which line do you want to delete: "
 
-            fi
+                # select menue displays all the lines of the choosen table
+                select choice in "${table_lines[@]}"; do
+                    # validate the user input to be a number that's greater than or equal 1 and less than or equal the length of the array
+                    if [[ $REPLY =~ ^[0-9]+$ && $REPLY -ge 1 && $REPLY -le ${#table_lines[@]} ]]; then
+                        # substracting 1 from the reply to match the array index that starts from zero
+                        selected_line="${table_lines[$((REPLY - 1))]}"
+                        # adding 2 to the REPLY to make the first choice matches the the first rec
+                        sed -i "$((REPLY + 2)) d" "$(pwd)"/"$option"
+                        echo "The following line has been deleted: $selected_line"
+                        break
+                    else
+                        echo "Invalid selection. Please enter a valid number."
+                    fi
+                done
+                break
+            }
         fi
-else
-    echo "Table doesn't exist"
-fi
-cd ../..
-./connect.sh
+    else
+        echo "Invalid choice. Please select a valid option."
+    fi
+done
+echo "Getting back to connect DBs menue"
+cd "../../"
+"./connect.sh"
